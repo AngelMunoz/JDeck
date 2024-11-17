1e7b729644532f4f58255b65a3023fc533d0504c
Angel D. Munoz <scythes.lol@gmail.com>
2024-11-17 12:43:21 -0600

    Update tests according to the newest changes on the error return type

--- JDeck.Tests/RequiredTests.fs
+++ JDeck.Tests/RequiredTests.fs
@@ -15,25 +15,25 @@ type RequiredTests() =
   member _.``JDeck can decode strings``() =
     match Decode.fromString("\"This is a string\"", Required.string) with
     | Ok value -> Assert.AreEqual("This is a string", value)
-    | Error err -> Assert.Fail(err)
+    | Error err -> Assert.Fail(err.message)
 
   [<TestMethod>]
   member _.``JDeck can decode true as boolean``() =
     match Decode.fromString("true", Required.boolean) with
     | Ok value -> Assert.IsTrue value
-    | Error err -> Assert.Fail(err)
+    | Error err -> Assert.Fail(err.message)
 
   [<TestMethod>]
   member _.``JDeck can decode false as boolean``() =
     match Decode.fromString("false", Required.boolean) with
     | Ok value -> Assert.IsFalse value
-    | Error err -> Assert.Fail(err)
+    | Error err -> Assert.Fail(err.message)
 
   [<TestMethod>]
   member _.``JDeck can decode characters``() =
     match Decode.fromString("\"a\"", Required.char) with
     | Ok value -> Assert.AreEqual('a', value)
-    | Error err -> Assert.Fail(err)
+    | Error err -> Assert.Fail(err.message)
 
   [<TestMethod>]
   member _.``JDeck fails to decode characters when strings are longer than 1``
@@ -42,7 +42,7 @@ type RequiredTests() =
     match Decode.fromString("\"ab\"", Required.char) with
     | Ok _ -> Assert.Fail()
     | Error err ->
-      Assert.AreEqual("Expecting a char but got \"ab\" of size: 2", err)
+      Assert.AreEqual("Expecting a char but got a string of size: 2", err.message)
 
   [<TestMethod>]
   member _.``JDeck can parse guids``() =
@@ -50,37 +50,37 @@ type RequiredTests() =
 
     match Decode.fromString($"\"{expected}\"", Required.guid) with
     | Ok actual -> Assert.AreEqual(expected, actual)
-    | Error err -> Assert.Fail(err)
+    | Error err -> Assert.Fail(err.message)
 
   [<TestMethod>]
   member _.``JDeck can parse nulls as unit``() =
     match Decode.fromString("null", Required.unit) with
     | Ok actual -> Assert.AreEqual((), actual)
-    | Error err -> Assert.Fail(err)
+    | Error err -> Assert.Fail(err.message)
 
   [<TestMethod>]
   member _.``JDeck can decode bytes``() =
     match Decode.fromString("10", Required.byte) with
     | Ok value -> Assert.AreEqual(10uy, value)
-    | Error err -> Assert.Fail(err)
+    | Error err -> Assert.Fail(err.message)
 
   [<TestMethod>]
   member _.``JDeck can decode ints``() =
     match Decode.fromString("10", Required.int) with
     | Ok value -> Assert.AreEqual(10, value)
-    | Error err -> Assert.Fail(err)
+    | Error err -> Assert.Fail(err.message)
 
   [<TestMethod>]
   member _.``JDeck can decode int64s``() =
     match Decode.fromString("1000", Required.int64) with
     | Ok value -> Assert.AreEqual(1000L, value)
-    | Error err -> Assert.Fail(err)
+    | Error err -> Assert.Fail(err.message)
 
   [<TestMethod>]
   member _.``JDeck can decode floats``() =
     match Decode.fromString("1000.50", Required.float) with
     | Ok value -> Assert.AreEqual(1000.50, value)
-    | Error err -> Assert.Fail(err)
+    | Error err -> Assert.Fail(err.message)
 
   [<TestMethod>]
   member _.``JDeck can decode date time``() =
@@ -96,7 +96,7 @@ type RequiredTests() =
         ),
         value
       )
-    | Error err -> Assert.Fail(err)
+    | Error err -> Assert.Fail(err.message)
 
   [<TestMethod>]
   member _.``JDeck can decode date time offset``() =
@@ -115,19 +115,20 @@ type RequiredTests() =
         ),
         value
       )
-    | Error err -> Assert.Fail(err)
+    | Error err -> Assert.Fail(err.message)
 
   [<TestMethod>]
   member _.``JDeck can decode objects``() =
     let json = """{ "Name": "John Doe", "Age": 30 }"""
 
     match
-      Decode.fromString(
+      Decode.validateFromString(
         json,
         (fun element -> validation {
 
           let! name = element |> Required.property "Name" Required.string
           and! age = element |> Required.property "Age" Required.int
+
           return {| name = name; age = age |}
         })
       )
@@ -136,13 +137,14 @@ type RequiredTests() =
       Assert.AreEqual("John Doe", value.name)
       Assert.AreEqual(30, value.age)
 
-    | Error err -> Assert.Fail(err |> String.concat ", ")
+    | Error err ->
+      err |> List.fold (fun acc e -> acc + e.message + ", ") "" |> Assert.Fail
 
   [<TestMethod>]
   member _.``JDeck can decode arrays``() =
     let json = """[1, 2, 3, 4, 5]"""
 
-    match Decode.fromString(json, array Required.int) with
+    match Decode.fromString(json, array(fun _ v -> Required.int v)) with
     | Ok value ->
       Assert.AreEqual(5, value.Length)
       Assert.AreEqual(1, value[0])
@@ -151,7 +153,7 @@ type RequiredTests() =
       Assert.AreEqual(4, value[3])
       Assert.AreEqual(5, value[4])
 
-    | Error err -> Assert.Fail(err)
+    | Error err -> Assert.Fail(err.message)
 
   [<TestMethod>]
   member _.``JDeck can decode nested objects``() =
@@ -159,7 +161,7 @@ type RequiredTests() =
       """{ "Name": "John Doe", "Age": 30, "Address": { "City": "New York", "Country": "USA" } }"""
 
     match
-      Decode.fromString(
+      Decode.validateFromString(
         json,
         (fun element -> validation {
 
@@ -170,18 +172,15 @@ type RequiredTests() =
             element
             |> Required.property
               "Address"
-              (fun address ->
-                validation {
-                  let! city =
-                    address |> Required.property "City" Required.string
+              (fun address -> result {
+                let! city =
+                  address |> Required.property "City" Required.string
 
-                  and! country =
-                    address |> Required.property "Country" Required.string
+                and! country =
+                  address |> Required.property "Country" Required.string
 
-                  return {| city = city; country = country |}
-                }
-                |> Result.mapError(String.concat ", ")
-              )
+                return {| city = city; country = country |}
+              })
 
           return {|
             name = name
@@ -196,7 +195,8 @@ type RequiredTests() =
       Assert.AreEqual(30, value.age)
       Assert.AreEqual("New York", value.address.city)
       Assert.AreEqual("USA", value.address.country)
-    | Error err -> Assert.Fail(err |> String.concat ", ")
+    | Error err ->
+      err |> List.fold (fun acc e -> acc + e.message + ", ") "" |> Assert.Fail
 
   [<TestMethod>]
   member _.``JDeck can decode nested arrays``() =
@@ -204,7 +204,7 @@ type RequiredTests() =
       """{ "Name": "John Doe", "Age": 30, "Addresses": [ { "City": "New York", "Country": "USA" }, { "City": "London", "Country": "UK" } ] }"""
 
     match
-      Decode.fromString(
+      Decode.validateFromString(
         json,
         (fun element -> validation {
 
@@ -215,20 +215,15 @@ type RequiredTests() =
             element
             |> Required.property
               "Addresses"
-              (array
-                (fun address ->
-                  validation {
-                    let! city =
-                      address |> Required.property "City" Required.string
+              (array(fun _ address -> result {
+                let! city =
+                  address |> Required.property "City" Required.string
 
-                    and! country =
-                      address |> Required.property "Country" Required.string
+                and! country =
+                  address |> Required.property "Country" Required.string
 
-                    return {| city = city; country = country |}
-                  }
-                  |> Result.mapError(String.concat ", ")
-                )
-              )
+                return {| city = city; country = country |}
+              }))
 
           return {|
             name = name
@@ -246,4 +241,5 @@ type RequiredTests() =
       Assert.AreEqual("USA", value.addresses[0].country)
       Assert.AreEqual("London", value.addresses[1].city)
       Assert.AreEqual("UK", value.addresses[1].country)
-    | Error err -> Assert.Fail(err |> String.concat ", ")
+    | Error err ->
+      err |> List.fold (fun acc e -> acc + e.message + ", ") "" |> Assert.Fail
